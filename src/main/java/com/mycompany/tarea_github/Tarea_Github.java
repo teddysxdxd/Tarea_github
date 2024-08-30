cc45d2e07906956d38006cd45d9fa79ef5d095bd
teddy <tedocastel@gmail.com>
2024-08-29 20:45:45 -0600

    Prueba0.1

---
+++ src/main/java/com/mycompany/tarea_github/Tarea_Github.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tarea_github;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Tarea_Github {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Wodfsfdrld!");
+    }
+}
